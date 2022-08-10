b8e151388dffe398f3e4b88383ae0d42cd2a807f
김재혁 <gimjaehyeog@gimjaehyeog-ui-MacBookPro.local>
2022-08-11 01:52:46 +0900

    1.2 Router Setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppRouter from "./Router";
+import Router from "./Router";
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
