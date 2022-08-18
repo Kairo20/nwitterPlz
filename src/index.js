f63e66c7079a645abb0e4db0b8d05e5f02b82c22
Kairo20 <jjangg0301@naver.com>
2022-08-18 19:29:16 +0900

    4.2 Uploading

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import firebase from "firebase/compat/app";
 
-console.log(firebase);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
-import { dbService } from "../fbase";
+import { dbService, storageService } from "../fbase";
+import { v4 as uuidv4 } from "uuid";
 import React, { useEffect, useState } from "react";
 import Nweet from "../components/Nweet";
 
@@ -22,12 +23,16 @@ const Home = ({ userObj }) => {
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
+    const fileRef = storageService.ref().child(`#${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    console.log(response);
+    /* 
     await dbService.collection("nweets").add({
       text: nweet,
       createdAt: Date.now(),
       creatorId: userObj.uid,
     });
-    setNweet("");
+    setNweet(""); */
   };
   const onChange = (event) => {
     setNweet(event.target.value);
