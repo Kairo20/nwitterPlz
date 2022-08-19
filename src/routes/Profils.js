9ba25f81349cd22eff9d594f3ca08e9a31779a91
Kairo20 <jjangg0301@naver.com>
2022-08-20 08:38:19 +0900

    5.1 Update Profile

--- src/routes/Profils.js
+++ src/routes/Profils.js
@@ -1,9 +1,10 @@
 import { authService, dbService } from "../fbase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
@@ -14,15 +15,33 @@ const Profile = ({ userObj }) => {
       .where("creatorId", "==", userObj.uid) //creatorId가 userObj.uid인 것들을
       .orderBy("createdAt") //시간순서로
       .get(); //가져온다
-
     console.log(nweets.docs.map((doc) => doc.data()));
   };
-
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+  const onChange = (event) => {
+    setNewDisplayName(event.target.value);
+  };
   useEffect(() => {
     getMyNweets();
   }, []);
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+          onChange={onChange}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}> Log Out</button>
     </>
   );
